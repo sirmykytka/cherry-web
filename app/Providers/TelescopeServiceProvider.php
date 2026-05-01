bba8fc6c7363c71559c3046468d94444264a8aab
sirmykytka <kyrychenko.m.g@gmail.com>
2026-05-01 22:41:39 +0300

    feat: migrate database and enable night theme

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -16,7 +16,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     public function register(): void
     {
-        // Telescope::night();
+        Telescope::night();
 
         $this->hideSensitiveRequestDetails();
 
